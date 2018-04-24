7823386eff478d813aff21944f6bb13345524cae
briecodes <hello@brie.work>
2018-04-23 20:30:41 -0400

    form and results defined.

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    # "Team Name: #{@name}"
+    # "Coach: #{@coach}"
+    # "Point Guard: #{@pg}"
+    # "Shooting Guard: #{@sg}"
+    # "Small Forward: #{@sf}"
+    # "Power Forward: #{@pf}"
+    # "Center: #{@c}"
+
+    erb :team
+  end
 
 end
